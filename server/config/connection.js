e34f4bd8cc9e795f9a8a654a5f6e34545b715883
Bruno Calderon <slardaromg@gmail.com>
2021-10-06 20:34:10 -0400

    finishing app

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ABCPRODUCTS', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Bruno-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
